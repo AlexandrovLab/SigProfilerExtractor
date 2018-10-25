b8a3eb6251faa650162855d6d114b5f6316abec4
mishu <mishugeb1@hotmail.com>
2018-10-24 18:05:48 -0700

    Signature_Leakage_Module_added

---
+++ source/sigLeakage.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 14:33:37 2018
+
+@author: mishugeb
+"""
+from numpy import linalg as LA
+import numpy as np
+from scipy.optimize import minimize
+import math
+from scipy import spatial
+
+mat = scipy.io.loadmat('21_breast_WGS_substitutions.mat')
+mat = extract_input(mat)
+genomes = mat[1]
+
+W, H = nmf(genomes, 10)
+
+Hnew = np.zeros(H.shape)
+
+    
+def parameterized_objective2_custom(x, signatures, samples):
+    rec = np.dot(signatures, x)
+    y = LA.norm(samples-rec)
+    return y
+
+
+def constraints1(x, samples):
+    sumOfSamples=np.sum(samples, axis=0)
+    #print(sumOfSamples)
+    #print(x)
+    result = sumOfSamples-(np.sum(x))
+    #print (result)
+    return result
+
+
+def create_bounds(idxOfZeros, samples, numOfSignatures):
+    total = np.sum(samples)
+    b = (0.0, float(total))
+    lst =[b]*numOfSignatures
+    
+    for i in idxOfZeros:
+        lst[i] = (0.0, 0.0) 
+    
+    return lst 
+
+def cos_sim(a, b):
+	"""Takes 2 vectors a, b and returns the cosine similarity according 
+	to the definition of the dot product
+    
+    Dependencies: 
+        *Requires numpy library. 
+        *Does not require any custom function (constructed by me)
+        
+    Required by:
+        * pairwise_cluster_raw
+	"""
+	dot_product = np.dot(a, b)
+	norm_a = np.linalg.norm(a)
+	norm_b = np.linalg.norm(b)
+	return dot_product / (norm_a * norm_b)
+
+
+
+
+# to do the calculation, we need: W, samples(one column), H for the specific sample, tolerence
+
+
+
+def remove_all_single_signatures(W, H, genomes):
+    # make the empty list of the successfull combinations
+    successList = [0,[],0] 
+    # get the cos_similarity with sample for the oringinal W and H[:,i]
+    originalSimilarity= cos_sim(genomes, np.dot(W, H))
+    # make the original exposures of specific sample round
+    oldExposures = np.round(H)
+    
+    # set the flag for the while loop
+    Flag = True
+    # The while loop starts here
+    while Flag: 
+        
+        # get the list of the indices those already have zero values
+        if len(successList[1]) == 0:
+            initialZerosIdx = list(np.where(oldExposures==0)[0]) 
+            #get the indices to be selected
+            selectableIdx = list(np.where(oldExposures>0)[0]) 
+        elif len(successList[1]) > 1: 
+            initialZerosIdx = list(np.where(successList[1]==0)[0]) 
+            #get the indices to be selected
+            selectableIdx = list(np.where(successList[1]>0)[0])
+        else:
+            print("iteration is completed")
+            #break
+        
+        
+        # get the total mutations for the given sample
+        maxmutation = round(np.sum(H))
+        
+        # new signature matrix omiting the column for zero
+        #Winit = np.delete(W, initialZerosIdx, 1)
+        Winit = W[:,selectableIdx]
+        
+        # set the initial cos_similarity
+        record  = [0.11, []] 
+        # get the number of current nonzeros
+        l= Winit.shape[1]
+        
+        for i in range(l):
+            #print(i)
+            loopSelection = list(range(l))
+            del loopSelection[i]
+            #print (loopSelection)
+            W1 = Winit[:,loopSelection]
+           
+            
+            #initialize the guess
+            x0 = np.random.rand(l-1, 1)*maxmutation
+            x0= x0/np.sum(x0)*maxmutation
+            
+            #set the bounds and constraints
+            bnds = create_bounds([], samples, W1.shape[1]) 
+            cons1 ={'type': 'eq', 'fun': constraints1, 'args':[samples]} 
+            
+            #the optimization step
+            sol = minimize(parameterized_objective2_custom, x0, args=(W1, samples),  bounds=bnds, constraints =cons1, tol=1e-30)
+            
+            #print (sol.success)
+            #print (sol.x)
+            
+            #convert the newExposure vector into list type structure
+            newExposure = list(sol.x)
+            
+            #insert the loopZeros in its actual position 
+            newExposure.insert(i, 0)
+            
+            #insert zeros in the required position the newExposure matrix
+            initialZerosIdx.sort()
+            for zeros in initialZerosIdx:
+                newExposure.insert(zeros, 0)
+            
+            # get the maximum value the new Exposure
+            maxcoef = max(newExposure)
+            idxmaxcoef = newExposure.index(maxcoef)
+            
+            newExposure = np.round(newExposure)
+            
+            
+            if np.sum(newExposure)!=maxmutation:
+                newExposure[idxmaxcoef] = round(newExposure[idxmaxcoef])+maxmutation-sum(newExposure)
+                
+            
+            newSample = np.dot(W, newExposure)
+            newSimilarity = cos_sim(genomes, newSample) 
+             
+            difference = originalSimilarity - newSimilarity
+            #print(originalSimilarity)
+            #print(newSample)
+            print(newExposure)
+            print(newSimilarity)
+            
+            #print(difference)
+            #print (newExposure)
+            #print (np.round(H))
+            #print ("\n\n")
+             
+            if difference<record[0]:
+                record = [difference, newExposure, newSimilarity]
+            
+            
+        print ("This loop's selection is {}".format(record))
+        
+        if record[0]>0.01:   
+            Flag=False
+        elif len(record[1][np.nonzero(record[1])])==1:
+            successList = record 
+            Flag=False
+        else:
+            successList = record
+        #print("The loop selection is {}".format(successList))
+        
+        #print (Flag)
+        #print ("\n\n")
+    
+    print ("The final selection is {}".format(successList))
+    
+    if len(successList[1])==0:
+        successList = [0.0, oldExposures, originalSimilarity]
+    
+    return successList[1]
+    
+
+
+
+    
+
+
+
+
