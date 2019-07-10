151f69d699dc82f41843e1c84909d8fa4dd69dbd
mishu <mishugeb1@hotmail.com>
2019-07-10 11:48:29 -0700

    updates

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 11:45:38 2019
+
+@author: mishugeb
+"""
+
+from sigproextractor import sigpro as sig
+data = sig.importdata("text")
