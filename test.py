a511f5ef8da6cc49f296bc42251bc8dee3586136
Mike Vella <mvella@nvidia.com>
2019-07-25 20:37:46 +0100

    Fix CI bug

--- test.py
+++ test.py
@@ -5,7 +5,10 @@ Created on Wed Jul 10 11:45:38 2019
 
 @author: mishugeb
 """
-
 from sigproextractor import sigpro as sig
-data = sig.importdata("text")
-sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3)
+def main():
+    data = sig.importdata("text")
+    sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3)
+
+if __name__ == '__main__':
+    main()
