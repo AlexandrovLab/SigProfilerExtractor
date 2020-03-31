72009c5f9480fb4508d10a0951bf3495cbb56b72
mishu <mishugeb1@hotmail.com>
2020-03-31 00:14:12 -0700

    parameter names changed

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ Created on Wed Jul 10 11:45:38 2019
 from SigProfilerExtractor import sigpro as sig
 def main():
     data = sig.importdata("text")
-    sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3)
+    sig.sigProfilerExtractor("text", "example_output", data, minimum_signatures=1, maximum_signatures=2, nmf_replicates=3)
 
 if __name__ == '__main__':
     main()
