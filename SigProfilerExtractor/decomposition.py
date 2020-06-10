d06f429d14f67b7c73c443d01a0cab3327147f3a
mishu <mishugeb1@hotmail.com>
2020-06-09 23:31:10 -0700

    decomposition plot added

--- SigProfilerExtractor/decomposition.py
+++ SigProfilerExtractor/decomposition.py
@@ -14,7 +14,7 @@ import os
 
 
 
-def decompose(signatures, activities, samples, output, nnls_add_penalty=0.05, nnls_remove_penalty=0.01, initial_remove_penalty=0.05, genome_build="GRCh37", refit_denovo_signatures=True, verbose=False):
+def decompose(signatures, activities, samples, output, nnls_add_penalty=0.05, nnls_remove_penalty=0.01, initial_remove_penalty=0.05, genome_build="GRCh37", refit_denovo_signatures=True, make_decomposition_plots=True, verbose=False):
 
     
     """
