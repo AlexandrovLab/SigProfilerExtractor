4fcec1d0c42a64d79224b95bfd415ce5f498c9ee
mdbarnesUCSD <mdbarnes@ucsd.edu>
2020-07-11 21:34:08 -0700

    SBS96 support for signature version, custom textbox, and margin cropping

--- SigProfilerExtractor/PlotDecomposition_SBS96.py
+++ SigProfilerExtractor/PlotDecomposition_SBS96.py
@@ -4,12 +4,14 @@ Created: February 21, 2020
 @author: Mark Barnes
 """
 import reportlab
+import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter, A4, landscape
 from reportlab.lib import utils
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 import SigProfilerExtractor as cosmic
+from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
 # imports for dashed line
 from reportlab.lib.colors import black
 paths = cosmic.__path__[0]
@@ -164,7 +166,7 @@ def plot_6_plus(bases, output_path, project, c_draw):
 # Parameters:
 #	c_draw 		- (Canvas) The canvas to draw the graph decomposition on.
 #	statistics 	- (Pandas Dataframe) Dataframe w/ calculations
-def draw_statistics(c_draw, statistics):
+def draw_statistics(c_draw, statistics, sig_version, custom_text):
 	cos_sim = statistics["Cosine Similarity"][0]
 	cor_coeff = statistics["Correlation Coefficient"][0]
 	l1_norm_percent = statistics["L1 Norm %"][0]
@@ -176,12 +178,19 @@ def draw_statistics(c_draw, statistics):
 		"Cosine Similarity: " + str(cos_sim))
 	c_draw.drawString(WIDTH_GAP+15, LAYOUT_2_TEXT[1][Y_COORD]-100, \
 		"Correlation: " + str(cor_coeff))
-	c_draw.drawString(WIDTH_GAP+15, LAYOUT_2_TEXT[1][Y_COORD]-110, \
+	c_draw.drawString(WIDTH_GAP+105, LAYOUT_2_TEXT[1][Y_COORD]-90, \
 		"L1 Error %: " + str(l1_norm_percent) + "%")
-	c_draw.drawString(WIDTH_GAP+15, LAYOUT_2_TEXT[1][Y_COORD]-120, \
+	c_draw.drawString(WIDTH_GAP+105, LAYOUT_2_TEXT[1][Y_COORD]-100, \
 		"L2 Error %: " + str(l2_norm_percent) + "%")
-	c_draw.drawString(WIDTH_GAP+15, LAYOUT_2_TEXT[1][Y_COORD]-130, \
+	c_draw.drawString(WIDTH_GAP+195, LAYOUT_2_TEXT[1][Y_COORD]-90, \
 		"KL Divergence: " + str(kl_divergence))
+
+	if sig_version is not None:
+		c_draw.drawString(WIDTH_GAP+195, LAYOUT_2_TEXT[1][Y_COORD]-100, \
+			"Signature Version: " + str(sig_version))
+	if custom_text is not None:
+		c_draw.drawString(WIDTH_GAP+15, LAYOUT_2_TEXT[1][Y_COORD]-120, \
+			str(custom_text))
 
 # Helper function to resize bracket depending on number of bases plotted
 # Parameters:
@@ -189,14 +198,71 @@ def draw_statistics(c_draw, statistics):
 #	c_draw 		- (Canvas) The canvas to draw the graph decomposition on.
 def draw_bracket(num_bases, c_draw):
     paths = cosmic.__path__[0]
-    
+
     num_plts = num_bases - 1
-    
+
     if(num_bases >= 5):
         num_plts = 4
-        
+
     c_draw.drawImage(paths+"/src/Accolade_fermante.png", MID_WIDTH_LETTER - 15, BRACKET_SIZES[num_plts][0], width = 20, height = BRACKET_SIZES[num_plts][1], mask='auto')
 
+# Helper function to remove the margins from the PlotDecomposition pdf
+# Parameters:
+#	pdf_to_edit 	- (String)	The path to the uncropped decomposition plot
+#	output_name 	- (String)	The name to save the cropped output as
+#	num_bases		- (Integer)	The number of signatures the sample is composed of
+def crop_margins(pdf_to_edit, output_name, num_bases):
+	pdf_file = PdfFileReader(open(pdf_to_edit, "rb"))
+	page = pdf_file.getPage(0)
+	writer = PdfFileWriter()
+	if (num_bases == 1):
+		page.mediaBox.lowerRight = (792,150)
+		page.mediaBox.lowerLeft = (0,150)
+		page.mediaBox.upperRight = (792,402)
+		page.mediaBox.upperLeft = (0,402)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
+	elif (num_bases == 2):
+		page.mediaBox.lowerRight = (792,150)
+		page.mediaBox.lowerLeft = (0,150)
+		page.mediaBox.upperRight = (792,422)
+		page.mediaBox.upperLeft = (0,422)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
+	elif (num_bases == 3):
+		page.mediaBox.lowerRight = (792,150)
+		page.mediaBox.lowerLeft = (0,150)
+		page.mediaBox.upperRight = (792,462)
+		page.mediaBox.upperLeft = (0,462)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
+	elif (num_bases == 4):
+		page.mediaBox.lowerRight = (792,112)
+		page.mediaBox.lowerLeft = (0,112)
+		page.mediaBox.upperRight = (792,498)
+		page.mediaBox.upperLeft = (0,498)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
+	elif (num_bases == 5):
+		page.mediaBox.lowerRight = (792,75)
+		page.mediaBox.lowerLeft = (0,75)
+		page.mediaBox.upperRight = (792,537)
+		page.mediaBox.upperLeft = (0,537)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
+	elif (num_bases > 5):
+		page.mediaBox.lowerRight = (792,55)
+		page.mediaBox.lowerLeft = (0,55)
+		page.mediaBox.upperRight = (792,537)
+		page.mediaBox.upperLeft = (0,537)
+		writer.addPage(page)
+		with open(output_name, "wb") as out_f:
+			writer.write(out_f)
 
 # Parameters:
 #   de_novo_name 	- (String) The name of the denovo signature.
@@ -208,7 +274,7 @@ def draw_bracket(num_bases, c_draw):
 #	statistics		- (Pandas Dataframe) If reconstructing, then include statistics.
 # Output:
 #   A graph of the de_novo signature's decomposition.
-def gen_plot(de_novo_name, basis_names, output_path, project, c, reconstruction, statistics):
+def gen_plot(de_novo_name, basis_names, output_path, project, c, reconstruction, statistics, sig_version, custom_text):
 
     # THIS IS THE RIGHT SIDE, IT CHANGES
 	num_bases = len(basis_names)
@@ -233,7 +299,7 @@ def gen_plot(de_novo_name, basis_names, output_path, project, c, reconstruction,
 		c.drawString(WIDTH_GRAPH - WIDTH_GAP - 25, LAYOUT_2_TEXT[0][Y_COORD], "Original")
 		c.drawImage(recon_png, WIDTH_GAP, LAYOUT_2_GRAPH[1][Y_COORD], width=WIDTH_GRAPH, height=HEIGHT_GRAPH)
 		c.drawString(WIDTH_GRAPH - WIDTH_GAP - 45, LAYOUT_2_TEXT[1][Y_COORD], "Reconstructed")
-		draw_statistics(c, statistics)
+		draw_statistics(c, statistics, sig_version, custom_text)
 
 		# Draw dashed line
 		c.setLineWidth(2)
@@ -253,9 +319,11 @@ def gen_plot(de_novo_name, basis_names, output_path, project, c, reconstruction,
 	c.showPage()
 
 
-def gen_decomposition(denovo_name, basis_names, weights, output_path, project, reconstruction, statistics):
-
-	c = canvas.Canvas(output_path+"/"+denovo_name+"_decomposition_"+project+".pdf", pagesize=letter)
+def gen_decomposition(denovo_name, basis_names, weights, output_path, project, \
+	reconstruction, statistics,  sig_version=None, custom_text=None):
+	output_plot_to_crop = output_path+"/"+denovo_name+"_decomposition_"+project+"_to_crop.pdf"
+	output_plot_cropped = output_path+"/"+denovo_name+"_decomposition_"+project+".pdf"
+	c = canvas.Canvas(output_plot_to_crop, pagesize=letter)
 	c.setPageSize(landscape(letter))
 	c.setFont("Arial-Bold", 7.19)
 
@@ -270,6 +338,12 @@ def gen_decomposition(denovo_name, basis_names, weights, output_path, project, r
 		basis_plots[j][1] = float(basis_plots[j][1].strip("%"))
 	sorted_list = sorted(basis_plots, key=lambda tup: tup[1], reverse=True)
 
-	gen_plot(denovo_name, sorted_list, output_path, project, c, reconstruction, statistics)
+	gen_plot(denovo_name, sorted_list, output_path, project, c, reconstruction, \
+		statistics, sig_version, custom_text)
 
 	c.save()
+
+	crop_margins(output_plot_to_crop, output_plot_cropped, len(basis_names))
+
+	if os.path.exists(output_plot_to_crop):
+			os.remove(output_plot_to_crop)
