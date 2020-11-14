ab2a4f3aa275b4615b9168f3288693bd8293c2a9
mishu <mishugeb1@hotmail.com>
2020-11-14 00:41:36 -0800

    windows_compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 import shutil
 import os
+import sys
+import subprocess
 
 #remove the dist folder first if exists
 if os.path.exists("dist"):
@@ -24,19 +26,7 @@ Update = 'Heirarchy option deleted, clustering deleted and signatures orders by 
     fh = open(filename, 'w')
     fh.write(cnt % {'version': VERSION,})
     fh.close()
-    
-write_version_py()
-setup(name='SigProfilerExtractor',
-      version=VERSION,
-      description='Extracts mutational signatures from mutational catalogues',
-      long_description=long_description,
-      long_description_content_type='text/markdown',  # This is important!	
-      url="https://github.com/AlexandrovLab/SigProfilerExtractor.git",
-      author='S Mishu Ashiqul Islam',
-      author_email='m0islam@ucsd.edu',
-      license='UCSD',
-      packages=['SigProfilerExtractor'],
-      install_requires=[
+requirements=[
           'matplotlib>=3.3.0',
           'scipy>=1.3.3',
           'torch==1.5.1',
@@ -52,6 +42,40 @@ setup(name='SigProfilerExtractor',
           'reportlab>=3.5.42',
           'PyPDF2>=1.26.0',
       'xlrd>=1.0.0'
-           ],
+           ]
+
+operating_system = sys.platform   
+if operating_system  in ['win32','cygwin','windows']:
+    requirements.remove('matplotlib>=3.3.0')
+    requirements.remove('torch==1.5.1')
+    print('Trying to install pytorch!')
+    code = 1
+    try:
+        code = subprocess.call(['pip', 'install', 'torch===1.5.1',  '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+        if code != 0:
+            raise Exception('Torch  instalation failed !')
+    except:
+        try:
+            code = subprocess.call(['pip3', 'install', 'torch===1.5.1',  '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+            if code != 0:
+                raise Exception('Torch instalation failed !')
+        except:
+            print('Failed to install pytroch, please install pytroch manually be following the simple instructions over at: https://pytorch.org/get-started/locally/')
+    if code == 0:
+        print('Successfully installed pytorch version! (If you need the GPU version, please install it manually, checkout the mindsdb docs and the pytroch docs if you need help)')
+    
+    
+write_version_py()
+setup(name='SigProfilerExtractor',
+      version=VERSION,
+      description='Extracts mutational signatures from mutational catalogues',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!	
+      url="https://github.com/AlexandrovLab/SigProfilerExtractor.git",
+      author='S Mishu Ashiqul Islam',
+      author_email='m0islam@ucsd.edu',
+      license='UCSD',
+      packages=['SigProfilerExtractor'],
+      install_requires=requirements,
       include_package_data=True,      
       zip_safe=False)
