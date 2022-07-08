1a9500c79cfc77e0e1a2daf56760ef4685c9f3f8
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-07-08 14:39:43 -0700

    updates to version.py and sigpro.py

--- SigProfilerExtractor/version.py
+++ SigProfilerExtractor/version.py
@@ -1,7 +1,8 @@
 
 # THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
-short_version = '1.1.6'
-version = '1.1.6'
-Update = 'Update normalization, selection and default NMF replicates'
+short_version = '1.1.8'
+version = '1.1.8'
+Update = 'Adding SigProfilerAssignment version to Job Metadata file 
+ Updating "opportunity_genome" parameter passed to SPA'
     
     
