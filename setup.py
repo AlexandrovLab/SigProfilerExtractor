8418de17ee3db1680863a1f16afeb491267694dc
mdbarnesUCSD <mdbarnes@ucsd.edu>
2023-09-15 12:07:09 -0700

    Update plotting requirement

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ requirements=[
           'pandas>=1.2.4', 
           'nimfa>=1.1.0', 
           'SigProfilerMatrixGenerator>=1.2.17',
-          'sigProfilerPlotting>=1.3.15',
+          'sigProfilerPlotting>=1.3.16',
           'SigProfilerAssignment>=0.0.32',
           'pillow',
           'statsmodels>=0.9.0',
