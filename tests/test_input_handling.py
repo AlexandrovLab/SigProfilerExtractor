21f97a0ffd1d3d2c6fe90f44bae04b55ceb1c330
Kundu <mkundu@MKundu.lan>
2024-02-13 14:01:54 -0800

    checking columns and rows

--- tests/test_input_handling.py
+++ tests/test_input_handling.py
@@ -51,4 +51,6 @@ def test_output_text():
 
     # Check if the actual dataframe matches the expected dataframe
     #assert actual_content.equals(expected_content), "Dataframes are not equal."
-    assert actual_content.columns.tolist() == expected_content.columns.tolist(), "Column names are not the same."
+    #assert actual_content.columns.tolist() == expected_content.columns.tolist(), "Column names are not the same."
+    assert actual_content.index.equals(expected_content.index), "Row indices are not the same."
+
